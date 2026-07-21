e3273b25d0533a58b342e4b0b306d929a5d55c65
Ashutosh Singh <ashutosh9454718823@gmail.com>
2026-07-21 14:50:44 +0530

    added the css and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState,useEffect,useRef } from 'react'
+import { Chatbot } from 'supersimpledev'
+import aiProfileImage from './assets/ai.png'//we write./ to find file in same folder and if there is a folder inside which our file is locateded and that folder is inside same folder then we do ./fname/  
+import userProfileImage from './assets/user.png'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+ function ChatInput({ChatList,Newchat}){
+      const[InputText,setInputText]=useState("")
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
 
-      <div className="ticks"></div>
+      function saveInputText(event){
+        setInputText(event.target.value)//this replaeces old value to new value(text) inside inputtext.
+      }
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      function sentMsg(){
+        // console.log(InputText);
+        const ourMsg=[
+                    ...ChatList,
+                    {
+                        msg:InputText,
+                        sender:"user",
+                        id:crypto.randomUUID()
+                    }
+                ]
+        
+      const response=Chatbot.getResponse(InputText);
+      Newchat(
+                [
+                    ...ourMsg,//here the msg will be in form of list of obj
+                    {
+                        msg:response,
+                        sender:"ai",
+                        id:crypto.randomUUID()
+                    }
+                ]
+            );
+      setInputText("")
+    }
+          return(
+            <div className="input-container">
+              <input placeholder="Write here your qeary to the chatbot" size="30" onChange={saveInputText} value={InputText} className="chat-input"/>
+              <button onClick={sentMsg} className="sent-btn">Send
+                </button>
+            </div>
+          );
+        }
+   
+    function ChatMsg({msg,sender}){
+      
+       
+        return(
+         
+          <div className={sender==="user"?"usermsg":"aimsg"}>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+            {sender==="ai" && <img src={aiProfileImage} width="40"/>}
+
+            <div className="msgtext">
+              {msg}
+            </div>
+
+            {sender==="user" && <img src={userProfileImage} width="40"/>}
+          </div>
+      );
+    }
+   
+    function ChatMsges({ChatList}){
+      //Hooks:-
+      const ChatListRef=useRef(null);
+
+      useEffect(
+        ()=>{
+          const containerElem=ChatListRef.current;
+          if(containerElem){
+            containerElem.scrollTop=containerElem.scrollHeight;
+          }
+        },[ChatList] 
+      );
+ 
+
+
+        return(  
+          <div className="displaymsgcontainer" ref={ChatListRef}>
+
+               { ChatList.map((Chat)=>{
+                    return(
+                        <ChatMsg 
+                            msg={Chat.msg}
+                            sender={Chat.sender}
+                            key={Chat.id}
+                        />
+                    );
+                }
+            )
+            
+        }
+        </div>
+
+    );
 }
+
+function App(){
+        const[ChatList,Newchat]=useState([{msg:"hey bro",sender:"user",id:"id1"},
+    {msg:"fine mate",sender:"ai",id:"id2"}])//it can store any type of value but gives only array.
+        return(
+          <div className="App-container">
+
+            <ChatMsges ChatList={ChatList} />
+            <ChatInput ChatList={ChatList} Newchat={Newchat}/>
+          </div>
+        );
+      }
 
 export default App
